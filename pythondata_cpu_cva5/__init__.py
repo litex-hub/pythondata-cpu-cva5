ea1075d60c82191a1391ad00aa55be916bb37ab7
LiteX Robot <robot@timvideos.us>
2022-05-30 19:56:00 +0000

    Updating pythondata-cpu-cva5 to 0.0.post649
    Updated data to v0.0-507-g3239e20 based on 3239e20360993151f435fc2f5a567e09b3f185ad from https://github.com/openhwgroup/cva5.
    > commit 3239e20360993151f435fc2f5a567e09b3f185ad
    > Merge: b4d6a9f ce38554
    > Author: Mike Thompson <mike@openhwgroup.org>
    > Date:   Thu May 26 13:17:14 2022 -0400
    >
    >     Merge pull request #5 from e-matthews/minor-fixes
    >
    >     Minor fixes
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cva5/__init__.py
+++ pythondata_cpu_cva5/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cva5"
 
 # Module version
-version_str = "0.0.post647"
-version_tuple = (0, 0, 647)
+version_str = "0.0.post649"
+version_tuple = (0, 0, 649)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post647")
+    pversion = V("0.0.post649")
 except ImportError:
     pass
 
@@ -35,11 +35,11 @@ Date:   Thu May 26 13:17:14 2022 -0400
 """
 
 # Tool version info
-tool_version_str = "0.0.post140"
-tool_version_tuple = (0, 0, 140)
+tool_version_str = "0.0.post142"
+tool_version_tuple = (0, 0, 142)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post140")
+    ptool_version = V("0.0.post142")
 except ImportError:
     pass
 
